8545dac098840e3ee6d944259d7b94d672c7ab2c
Leonardo <leonardocccamargo@hotmail.com>
2020-02-29 10:26:04 -0300

    cria backend frontend iniciais

---
+++ concliciador/src/main/java/com/equiperocket/concliciador/ConcliciadorApplication.java
@@ -0,0 +1,13 @@
+package com.equiperocket.concliciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcliciadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcliciadorApplication.class, args);
+	}
+
+}
---
+++ concliciador/src/test/java/com/equiperocket/concliciador/ConcliciadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equiperocket.concliciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcliciadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
