fd3acb69c724278a9fbaa4f371b614e86934475b
Matheus Mendes <festenmatheus.mh@gmail.com>
2020-02-29 12:30:20 -0300

    Altera menu lateral

--- frontend/conciliador-front/src/app/app.module.ts
+++ frontend/conciliador-front/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GuiaComponent } from './guia/guia.component';
+import { ConciliaComponent } from './concilia/concilia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GuiaComponent,
+    ConciliaComponent
   ],
   imports: [
     BrowserModule,
