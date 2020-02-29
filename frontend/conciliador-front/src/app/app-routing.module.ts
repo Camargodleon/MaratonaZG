c98dc06508099d359fb3086722afb9a330651ef5
Matheus Mendes <festenmatheus.mh@gmail.com>
2020-02-29 17:07:03 -0300

    configura template guia e conciliacao

--- frontend/conciliador-front/src/app/app-routing.module.ts
+++ frontend/conciliador-front/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GuiaComponent } from './guia/guia.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/guias', component: GuiaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
